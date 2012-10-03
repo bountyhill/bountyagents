7a22c0aeb7de44d4adaf79a328e5c832514d0783
radiospiel <eno@open-lab.org>
2012-10-03 21:58:12 +0200

    Moves (and simplifies!!) config code into setup.rb

--- agents/twirl/setup.rb
+++ agents/twirl/setup.rb
@@ -22,8 +22,22 @@ if TweetStream::VERSION !~ /2.1.0/
   W "Potentially unsupported TweetStream version", TweetStream::VERSION
 end
 
-TweetStream.configure(&twitter_config)
+# configure TweetStream
+
+# Returns a twitter oauth object for this instance.
+$oauth = Bountybase.config.twitter[Bountybase.instance] || begin
+  E "Cannot find twitter configuration for", Bountybase.instance
+  exit 1
+end
+
+TweetStream.configure do |config|
+  config.consumer_key       = $oauth["consumer_key"]
+  config.consumer_secret    = $oauth["consumer_secret"]
+  config.oauth_token        = $oauth["access_token"]
+  config.oauth_token_secret = $oauth["access_token_secret"]
+  config.auth_method        = :oauth
+end
+
+I "Configured TweetStream using", $oauth
 
 $client = TweetStream::Client.new
-I "Configured TweetStream using", twitter_config.call(OpenStruct.new)
-
